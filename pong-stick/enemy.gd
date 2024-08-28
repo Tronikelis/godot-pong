7b16316b3d418573b8f74e6c5472edaa28f36b93
Tronikel <contactdonatas@gmail.com>
2024-08-28 17:59:19 +0300

    add dumb bot (now he cant win everytime)

--- pong-stick/enemy.gd
+++ pong-stick/enemy.gd
@@ -1,7 +1,26 @@
 extends Node2D
 
-@export var pong_ball: Node
+@export var pong_ball: PongBall
+@export var speed := 100
 
 
-func _physics_process(_delta: float) -> void:
-	self.position.y = pong_ball.position.y
+func _physics_process(delta: float) -> void:
+	var direction := 0
+
+	if self.position.y < pong_ball.position.y:
+		direction = 1
+	else:
+		direction = -1
+
+	var add_y := speed * direction * delta
+	var abs_add_y: float = abs(add_y)
+
+	var abs_pong_y: float = abs(pong_ball.position.y)
+	var abs_self_y: float = abs(self.position.y)
+
+	var delta_y: float = abs(abs_pong_y - abs_self_y)
+
+	if delta_y < abs_add_y:
+		add_y = delta_y * direction
+
+	self.position.y += add_y
