16217e33894b978a7637adee0a7620ac3eef76c6
Tronikel <contactdonatas@gmail.com>
2024-08-29 20:16:25 +0300

    enemy score

--- score.gd
+++ score.gd
@@ -1,20 +1,20 @@
-extends Label
+class_name Score
+extends Node2D
+
+@onready var label: Label = $Label
+@export var increment_on: PongWall.Type
 
 
 func _ready() -> void:
-	self.text = "0"
+	label.text = "0"
 	SignalBus.pong_ball_collided.connect(self.on_ball_collided)
 
 
 func on_ball_collided(_ball: Area2D, area: PongWall) -> void:
-	if area.type == area.Type.PLAYER:
+	if area.type == increment_on:
 		self.incrementScore()
 
 
-func setText(new: String) -> void:
-	self.text = new
-
-
 func incrementScore() -> void:
-	var current := int(self.text)
-	self.setText(str(current + 1))
+	var current := int(label.text)
+	label.text = str(current + 1)
